42b7a45d99d895b947e91cdbdc2c7545a15ed1dc
Anurag Bhatt <anuragbhatt1805@gmail.com>
2024-06-23 01:20:50 +0530

    chore: Fix error handling and improve code readability in User.controller.js

--- src/controller/User.controller.js
+++ src/controller/User.controller.js
@@ -8,40 +8,40 @@ export const RegisterUser = AsyncHandler(async (req, res) => {
         const {name, email, username, password} = req.body;
 
         if (!name || !email || !username || !password) {
-            return res.status(400).json(ApiError(400, "Please provide all required fields"));
+            return res.status(400).json(new ApiError(400, "Please provide all required fields"));
         }
 
         const existingUser = await User.findOne({$or : [{email}, {username}]});
 
         if (existingUser) {
-            return res.status(400).json(ApiError(400, "User already exists"));
+            return res.status(400).json(new ApiError(400, "User already exists"));
         }
 
         if (!req?.files?.image[0]?.path){
-            return res.status(400).json(ApiError(400, "Please provide an profile picture"));
+            return res.status(400).json(new ApiError(400, "Please provide an profile picture"));
         }
 
         const image = await uploadOnCloudinary(req.files.image[0].path);
 
         if (!image) {
-            return res.status(500).json(ApiError(500, "Image upload failed"));
+            return res.status(500).json(new ApiError(500, "Image upload failed"));
         }
 
         const user = await User.create({
             name: name.trim(),
             email: email.trim().toLowerCase(),
-            username: username.trim().toUppercase(),
+            username: username.trim().toUpperCase(),
             password,
             image: image.url,
         })
 
         if (!user) {
-            return res.status(500).json(ApiError(500, "User registration failed"));
+            return res.status(500).json(new ApiError(500, "User registration failed"));
         }
 
         const {accessToken, refreshToken} = await generateToken(user._id);
 
-        const newUser = await User.findById(user._id).select("-password -__v -createdAt -updatedAt -_id");
+        const newUser = await User.findById(user._id).select("-password -__v -_id");
 
         const options = {
             httpOnly: true,
@@ -51,9 +51,9 @@ export const RegisterUser = AsyncHandler(async (req, res) => {
         return res.status(201)
             .cookie("accessToken", accessToken, options)
             .cookie("refreshToken", refreshToken, options)
-            .json(ApiResponse(201, newUser, "User registered successfully"));
+            .json(new ApiResponse(201, {...newUser?._doc, accessToken, refreshToken}, "User registered successfully"));
     } catch (error) {
         console.log(error);
-        return res.status(500).json(ApiError(500, "Internal Server Error"));
+        return res.status(500).json(new ApiError(500, "Internal Server Error"));
     }
 });
--- src/util/cloudinary.js
+++ src/util/cloudinary.js
@@ -1,6 +1,9 @@
 import { v2 as cloudinary } from "cloudinary";
 import fs from "fs";
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 cloudinary.config({
   cloud_name: process.env.CLOUD_APP_NAME,
   api_key: process.env.CLOUD_APP_KEY,
@@ -11,13 +14,14 @@ const uploadOnCloudinary = async (localFilePath) => {
   try {
     if (!localFilePath) return null;
 
-    const response = await cloudinary.uploader.upload(localFilePath, {
+    const response = await cloudinary.uploader.upload(`./${localFilePath}`, {
       resource_type: "auto",
     });
     fs.unlinkSync(localFilePath);
     return response;
-    
+
   } catch (error) {
+    console.log(error);
     fs.unlinkSync(localFilePath);
     return null;
   }
